48330487c1a606732f4cfd8df76eea56c9e29c9f
Nicholas Wengel <ninjaperson24119@gmail.com>
2023-08-31 17:40:04 -0600

    rm unnecessary date_format decorator, simplify filter by date

--- finance_data/filter_by_date.py
+++ finance_data/filter_by_date.py
@@ -3,22 +3,22 @@ import pandas as pd
 from finance_data_base import FinanceDataBase
 from constants import STANDARD_COLUMNS
 
-class FinanceDataCreditCard(FinanceDataBase):
-    def __init__(self, finance_data):
-        self.finance_data = finance_data
-
-class FinanceDataFilteredByDate:
-    # maps the first 3 characters of a month to its full name.
-    # e.g. "jan" -> "January"
+class FinanceDataFilteredByDate(FinanceDataBase):
+    # maps the first 3 characters of a month to its index.
+    # e.g. "jan" -> 1
     MONTH_MAPPING = {month.lower(): index for index, month in enumerate(calendar.month_abbr) if month}
     
-    def __init__(self, finance_data):
+    def __init__(self, finance_data, month, year):
         self.finance_data = finance_data
+        self.month = month
+        self.year = year
 
-    def filter_by_date(self, month, year):
-        month = self.MONTH_MAPPING[month[:3]]
+    def filter_by_date(self):
+        month = self.MONTH_MAPPING[self.month[:3]]
         day = calendar.monthrange(int(year), month)[1]
-        assert self.df[STANDARD_COLUMNS['DATE']].shape[0] == pd.to_datetime(self.df[STANDARD_COLUMNS['DATE']]).shape[0]
-        self.df[STANDARD_COLUMNS['DATE']] = pd.to_datetime(self.df[STANDARD_COLUMNS['DATE']])
         self.df = self.df[(self.df[STANDARD_COLUMNS['DATE']] >= f'{year}-{month}-1') & (self.df[STANDARD_COLUMNS['DATE']] <= f'{year}-{month}-{day}')]
         self.df = self.df.sort_values(by=STANDARD_COLUMNS['DATE'])
+
+    def postprocess(self):
+        super.postprocess()
+        self.filter_by_date()
