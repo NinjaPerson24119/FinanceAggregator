132469ecb7ff642693abc80aa7cc27b6faca38ca
Nicholas Wengel <ninjaperson24119@gmail.com>
2023-09-01 12:36:57 -0600

    elaborate that source is not from csv

--- finance_data/constants/columns.py
+++ finance_data/constants/columns.py
@@ -2,5 +2,6 @@ STANDARD_COLUMNS = {
     "DATE": "date",
     "NAME": "name",
     "AMOUNT": "amount",
+    # this column is not in the CSV, but is added by FinanceData based on the configuration
     "SOURCE": "source",
 }
