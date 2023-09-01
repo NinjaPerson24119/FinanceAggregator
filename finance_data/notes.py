9f497fa533bc2c6f8176afeb3969eb50ccf83263
Nicholas Wengel <ninjaperson24119@gmail.com>
2023-08-31 23:23:23 -0600

    migrate notes feature

--- finance_data/notes.py
+++ finance_data/notes.py
@@ -1,27 +1,29 @@
 from finance_data_base import FinanceDataBase
 from constants import STANDARD_COLUMNS
 from pydantic import BaseModel
+import pandas as pd
 
 class NotesConfig(BaseModel):
-    note: list[dict[str, list[str]]]
+    note: dict[str, list[str]]
 
 class FinanceDataWithNotes(FinanceDataBase):
     NOTES_COLUMN = 'notes'
 
-    def __init__(self, finance_data: FinanceDataBase, notes_config):
+    def __init__(self, finance_data: FinanceDataBase, notes_config: NotesConfig):
         self.finance_data = finance_data
         self.notes_config = notes_config
 
-    def prefill_notes_column(self):
-        # TODO
-        with open(os.path.join(config_dir, notes_prefill_filename), 'r') as f:
-            c = json.load(f)
-    
-        def notes_row(in_row):
-            notes_cell = ''
-            for note in c[EXTENDED_COLUMNS['NOTES']]:
-                for substring in c[EXTENDED_COLUMNS['NOTES']][note]:
-                    if substring.lower() in in_row[STANDARD_COLUMNS['NAME']].lower():
-                        notes_cell += f'{note} '
-            return notes_cell
-        self.df[EXTENDED_COLUMNS['NOTES']] = self.df.apply(lambda row: notes_row(row), axis=1)
+    def generate_notes_for_row(self, row: pd.Series):
+        notes = ''
+        for note, substrings in self.notes_config.items():
+            for substring in substrings:
+                if substring.lower() in row[STANDARD_COLUMNS['NAME']].lower():
+                    notes += f'{note} '
+        return notes
+
+    def add_and_prefill_notes(self):
+        self.df[self.NOTES_COLUMN] = self.df.apply(lambda row: self.generate_notes_for_row(row), axis=1)
+
+    def postprocess(self):
+        super().postprocess()
+        self.add_and_prefill_notes()
