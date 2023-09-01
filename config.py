0fbd1f84bc0871d167a390ed5d26d847f39d9e90
Nicholas Wengel <ninjaperson24119@gmail.com>
2023-09-01 02:41:30 -0600

    more fixes to schema, apparently have to explicitly assign None to Optional

--- config.py
+++ config.py
@@ -6,25 +6,25 @@ from typing import Optional
 class OutputConfig(BaseModel):
     path: str
     start_date: datetime.date
-    end_date: Optional[datetime.date]
+    end_date: Optional[datetime.date] = None
 
 class SourceConfig(BaseModel):
     path: str
 
     finance_data_config: FinanceDataConfig
-    negate_amount: Optional[bool]
-    combine_in_out_amount_config: Optional[CombineInOutColumnsConfig]
+    negate_amount: Optional[bool] = None
+    combine_in_out_amount_config: Optional[CombineInOutColumnsConfig] = None
 
     # source specific filters
-    filter_names_with_substrings: Optional[list[str]]
+    filter_names_with_substrings: Optional[list[str]] = None
     
 
 class AppConfig(BaseModel):
     output: OutputConfig
     sources: list[SourceConfig]
     
-    notes_config: Optional[NotesConfig]
-    category_config: Optional[CategoriesConfig]
+    notes_config: Optional[NotesConfig] = None
+    category_config: Optional[CategoriesConfig] = None
 
     # global filters
-    filter_names_with_substrings: Optional[list[str]]
+    filter_names_with_substrings: Optional[list[str]] = None
